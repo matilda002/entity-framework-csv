0a2d247317951fa8160a5164b3b04b4e442e3174
matilda002 <matilda0817@hotmail.com>
2024-01-16 19:12:55 +0100

    initial commit

---
+++ ef-csv.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ef_csv.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ef-csv/Program.cs
@@ -0,0 +1,26 @@
+﻿using ef_csv;
+using BloggingContext? db = new();
+
+string[] users = File.ReadAllLines($"Users.csv");
+string[] posts = File.ReadAllLines($"Posts.csv");
+string[] blogs = File.ReadAllLines($"Blogs.csv");
+
+// Filling in tables
+foreach (var u in users)
+{
+    string[] splitUsers = u.Split(';');
+    User user = new User
+    {
+        UserId = int.Parse(splitUsers[0]),
+        Username = splitUsers[1],
+        Email = splitUsers[2],
+        Password = splitUsers[3],
+    };
+    
+    db.Users.Add(user);
+} 
+
+db.SaveChanges();
+
+
+
